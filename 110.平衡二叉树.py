05e27372c918337f16c783970c1c8d23fb3d4386
yfj <17717092026@163.com>
2019-05-23 11:57:16 +0800

    Add on 20190523

---
+++ 110.平衡二叉树.py
@@ -0,0 +1,71 @@
+#
+# @lc app=leetcode.cn id=110 lang=python3
+#
+# [110] 平衡二叉树
+#
+# https://leetcode-cn.com/problems/balanced-binary-tree/description/
+#
+# algorithms
+# Easy (46.87%)
+# Likes:    115
+# Dislikes: 0
+# Total Accepted:    17.4K
+# Total Submissions: 36.5K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# 给定一个二叉树，判断它是否是高度平衡的二叉树。
+# 
+# 本题中，一棵高度平衡二叉树定义为：
+# 
+# 
+# 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+# 
+# 
+# 示例 1:
+# 
+# 给定二叉树 [3,9,20,null,null,15,7]
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 返回 true 。
+# 
+# 示例 2:
+# 
+# 给定二叉树 [1,2,2,3,3,null,null,4,4]
+# 
+# ⁠      1
+# ⁠     / \
+# ⁠    2   2
+# ⁠   / \
+# ⁠  3   3
+# ⁠ / \
+# ⁠4   4
+# 
+# 
+# 返回 false 。
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.isBalancedCore(root) != -1
+    def isBalancedCore(self,root):
+        if not root:
+            return 0
+        leftLen = self.isBalancedCore(root.left)
+        rigthLen = self.isBalancedCore(root.right)
+        if leftLen == -1 or rigthLen == -1 or abs(leftLen-rigthLen) > 1:
+            return -1
+        return max(leftLen,rigthLen)+1 
+        
+
