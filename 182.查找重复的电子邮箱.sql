a606cf4afc2081fb70840b59fad885f02775c468
Styfjion <fengjia_yang@sjtu.edu.cn>
2019-06-03 20:25:34 +0800

    Add on 20190603 (SQL Excercise)

---
+++ 182.查找重复的电子邮箱.sql
@@ -0,0 +1,46 @@
+--
+-- @lc app=leetcode.cn id=182 lang=mysql
+--
+-- [182] 查找重复的电子邮箱
+--
+-- https://leetcode-cn.com/problems/duplicate-emails/description/
+--
+-- database
+-- Easy (74.79%)
+-- Likes:    113
+-- Dislikes: 0
+-- Total Accepted:    27.6K
+-- Total Submissions: 36.9K
+-- Testcase Example:  '{"headers": {"Person": ["Id", "Email"]}, "rows": {"Person": [[1, "a@b.com"], [2, "c@d.com"], [3, "a@b.com"]]}}'
+--
+-- 编写一个 SQL 查询，查找 Person 表中所有重复的电子邮箱。
+-- 
+-- 示例：
+-- 
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- 
+-- 
+-- 根据以上输入，你的查询应返回以下结果：
+-- 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- 
+-- 
+-- 说明：所有电子邮箱都是小写字母。
+-- 
+--
+# Write your MySQL query statement below
+SELECT Email AS Email
+FROM Person
+GROUP BY Email
+HAVING count(Email) > 1;
+
---
+++ 183.从不订购的客户.sql
@@ -0,0 +1,59 @@
+--
+-- @lc app=leetcode.cn id=183 lang=mysql
+--
+-- [183] 从不订购的客户
+--
+-- https://leetcode-cn.com/problems/customers-who-never-order/description/
+--
+-- database
+-- Easy (61.16%)
+-- Likes:    93
+-- Dislikes: 0
+-- Total Accepted:    20.7K
+-- Total Submissions: 33.8K
+-- Testcase Example:  '{"headers": {"Customers": ["Id", "Name"], "Orders": ["Id", "CustomerId"]}, "rows": {"Customers": [[1, "Joe"], [2, "Henry"], [3, "Sam"], [4, "Max"]], "Orders": [[1, 3], [2, 1]]}}'
+--
+-- 某网站包含两个表，Customers 表和 Orders 表。编写一个 SQL 查询，找出所有从不订购任何东西的客户。
+-- 
+-- Customers 表：
+-- 
+-- +----+-------+
+-- | Id | Name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- 
+-- 
+-- Orders 表：
+-- 
+-- +----+------------+
+-- | Id | CustomerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- 
+-- 
+-- 例如给定上述表格，你的查询应返回：
+-- 
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+-- 
+-- 
+--
+# Write your MySQL query statement below
+SELECT Name AS Customers
+FROM Customers
+WHERE Customers.Id NOT IN
+(
+    SELECT CustomerId
+    FROM Orders
+);
+
