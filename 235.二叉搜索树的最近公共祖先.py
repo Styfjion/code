1cf891042de450bdd3e93a5a1089b649a124d0a5
Styfjion <fengjia_yang@sjtu.edu.cn>
2019-08-12 23:45:29 +0800

    Add on 20190812

---
+++ 235.二叉搜索树的最近公共祖先.py
@@ -0,0 +1,140 @@
+#
+# @lc app=leetcode.cn id=235 lang=python3
+#
+# [235] 二叉搜索树的最近公共祖先
+#
+# https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+#
+# algorithms
+# Easy (60.08%)
+# Likes:    143
+# Dislikes: 0
+# Total Accepted:    20.5K
+# Total Submissions: 33.9K
+# Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+#
+# 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+# 
+# 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x
+# 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+# 
+# 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+# 
+# 
+# 
+# 
+# 
+# 示例 1:
+# 
+# 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+# 输出: 6 
+# 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+# 
+# 
+# 示例 2:
+# 
+# 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+# 输出: 2
+# 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+# 
+# 
+# 
+# 说明:
+# 
+# 
+# 所有节点的值都是唯一的。
+# p、q 为不同节点且均存在于给定的二叉搜索树中。
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#自己答案
+class Solution:
+    result = None
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > q.val:
+            p,q = q,p
+        if root.val>=p.val and root.val<=q.val:
+            self.result = root
+            return self.result
+        elif root.val > q.val:
+            self.lowestCommonAncestor(root.left,p,q)
+        else:
+            self.lowestCommonAncestor(root.right,p,q)
+        return self.result
+    
+# 官方答案
+
+# 递归法
+class Solution2:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:    
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:    
+            return self.lowestCommonAncestor(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root
+
+# 迭代法
+class Solution3:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # Start from the root node of the tree
+        node = root
+
+        # Traverse the tree
+        while node:
+
+            # Value of current node or parent node.
+            parent_val = node.val
+
+            if p_val > parent_val and q_val > parent_val:    
+                # If both p and q are greater than parent
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                # If both p and q are lesser than parent
+                node = node.left
+            else:
+                # We have found the split point, i.e. the LCA node.
+                return node
+'''
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/two-sum/solution/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian--2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
