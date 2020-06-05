e4357dc3c78b7844f17687dc5baebbf4c6c820e6
Styfjion <33060090+Styfjion@users.noreply.github.com>
2020-06-05 19:36:53 +0800

    fix bug

--- path_sum_dfs_ii.c
+++ path_sum_dfs_ii.c
@@ -1,88 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "securec.h"
-#define min(a,b) (((a) < (b)) ? (a) : (b))
-#define MAX_N 100
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define MAX 100
 
 typedef struct {
     int val;
     int childNum;
-    int children[MAX_N];
+    int children[MAX];
 } Node;
 
-int gPathNum = 0;
-int gPaths[MAX_N][MAX_N+1] = {0};
-int gPath[MAX_N] = {0};    //最后一位存储数组长度
+int g_paths[MAX][MAX + 1] = {0};
+int g_result[MAX] = {0};
+int g_pathNum = 0;
 
-int cmp(const void *n1, const void *n2)
+void AddPath(Node *node, int level)
 {
-    int *pathA = (int *) n1;
-    int *pathB = (int *) n2;
-    int length = min(pathA[MAX_N], pathB[MAX_N]);
-    for (int i = 0; i < length; i++) {
-        if (pathA[i] != pathB[i]) {
-            return pathA[i] -  pathB[i];
-        }
-    }
-    return pathA[MAX_N] - pathB[MAX_N];
-}
-
-void AddPath(Node *node, int level) {
     if (node->childNum != 0) {
         return;
     }
-    memcpy_s(gPaths[gPathNum], sizeof(int) * MAX_N, gPath,  sizeof(int) * level);
-    gPaths[gPathNum][MAX_N] = level;
-    gPathNum++;
+    memcpy_s(g_paths[g_pathNum], MAX * sizeof(int), g_result, level * sizeof(int));
+    g_paths[g_pathNum][MAX] = level;
+    g_pathNum++;
 }
 
-void DFS(Node *nodes, int index, int level, int sum, int target) {
-    if (sum == target) {
-        AddPath(&nodes[index], level);
+void DFS(Node *tree, int index, int level, int sum, int target)
+{
+    if (target == sum) {
+        AddPath(&tree[index], level);
         return;
     }
     if (sum > target) {
         return;
     }
-    if (nodes[index].childNum == 0) {
+    if (tree[index].childNum == 0) {
         return;
     }
-    for (int i = 0; i < nodes[index].childNum; i++) {
-        int val = nodes[nodes[index].children[i]].val;
-        gPath[level] = val; 
-        DFS(nodes, nodes[index].children[i], level+1, sum+val, target);
+    for (int i = 0; i < tree[index].childNum; i++) {
+        int newIndex = tree[index].children[i];
+        g_result[level] = tree[newIndex].val;
+        DFS(tree, newIndex, level + 1, sum + tree[newIndex].val, target);
     }
 }
 
-
-void PrintPath()
+void PrintNode()
 {
-    for(int i = gPathNum - 1; i > -1; i--) {
-        int len = gPaths[i][MAX_N];
-        for (int j = 0; j < len - 1 ; j++) {
-            printf("%d ", gPaths[i][j]);
+    for (int i = 0; i < g_pathNum; i++) {
+        int len = g_paths[i][MAX];
+        for (int j = 0; j < len - 1; j++) {
+            printf("%d ", g_paths[i][j]);
         }
-        printf("%d\n", gPaths[i][len - 1]);
+        printf("%d\n", g_paths[i][len - 1]);
     }
 }
 
-int main(int argc, char const *argv[])
+int NodeCmp(const void *p1, const void *p2)
+{
+    int *ch1, *ch2;
+    ch1 = (int *)p1;
+    ch2 = (int *)p2;
+    int length = min(ch1[MAX], ch2[MAX]);
+    for (int i = 0; i < length; i++) {
+        if (ch1[i] != ch2[i]) {
+            return ch2[i] - ch1[i];
+        }
+    }
+    return ch2[MAX] - ch1[MAX];
+}
+
+/* 请按需拆分或重组函数，避免过高的圈复杂度 */
+int main(void)
 {
     int n, m, s;
     if (scanf_s("%d %d %d", &n, &m, &s) != 3) {
         return -1;
     }
-
-    Node *nodes = (Node *)malloc(n * sizeof(Node));
-    memset(nodes, 0, n * sizeof(Node));
-    
-        int i;
+    Node tree[n];
+    memset(tree, 0, sizeof(tree));
+    int i;
     int w;
     for (i = 0; i < n; i++) {
         if (scanf_s("%d", &w) != 1) {
             return -1;
         }
-        nodes[i].val = w;
+        // TODO: 处理权重
+        tree[i].val = w;
     }
 
     int parent, child;
@@ -92,34 +94,21 @@ int main(int argc, char const *argv[])
         if (scanf_s("%d %d", &parent, &childCnt) != 2) {
             return -1;
         }
-        nodes[parent].childNum = childCnt;
+        tree[parent].childNum = childCnt;
         for (j = 0; j < childCnt; j++) {
             if (scanf_s("%d", &child) != 1) {
                 return -1;
             }
-            nodes[parent].children[j] = child;
+            // TODO: 处理父子关系
+            tree[parent].children[j] = child;
         }
     }
-    int sum = nodes[0].val;
-    gPath[0] = sum;
-    DFS(nodes, 0, 1, sum, s);
-    qsort(gPaths, gPathNum - 1, sizeof(int) * (MAX_N + 1), cmp);
-    PrintPath();
+
+    int sum = tree[0].val;
+    g_result[0] = tree[0].val;
+    DFS(tree, 0, 1, sum, s);
+    qsort(g_paths, g_pathNum, (MAX + 1) * sizeof(int), NodeCmp);
+    PrintNode();
+    // TODO: 输出
     return 0;
 }
-
-/*
-输入：
-20 9 24
-10 2 4 3 5 10 2 18 9 7 2 2 1 3 12 1 8 6 2 2
-00 4 01 02 03 04
-02 1 05
-04 2 06 07
-03 3 11 12 13
-06 1 09
-07 2 08 10 
-16 1 15
-13 3 14 16 17
-17 2 18 19
-*/
-
